0d378f4f98137025263e465f3b4868f5bb9b684f
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-09-28 23:18:30 +0200

    feat: added supersedes option to builtin configs

---
+++ configs/svite-routify.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'Svite / Routify 2',
+    supersedes: ['routify2', 'routify'],
+    condition: ({ pkgjson }) => pkgjson.dependencies['@roxi/routify'],
+    config: () => {
+        const { readFileSync } = require('fs')
+        const html = readFileSync('./dist/index.html', 'utf-8')
+        const script = html.match(/src="\/(_assets\/index.\w+.js)"/)[1]
+        return {
+            script: `dist/${script}`,
+            entrypoint: 'dist/index.html',
+            inlineDynamicImports: true,
+            sitemap: '.routify/urlIndex.json',
+            output: 'dist',
+            eventName: 'app-loaded',
+        }
+    }
+}

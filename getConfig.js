a449de4a2763921dec5849e93ca106fb63b2be35
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-27 21:18:16 +0100

    fix: configent could break if used by 2+ libs

--- getConfig.js
+++ getConfig.js
@@ -3,7 +3,7 @@ const { configent } = require('configent')
 const defaults = require('./defaults')
 
 const getConfig = async input => {    
-    return configent(defaults, input, {useDetectDefaults: true})
+    return configent(defaults, input, {useDetectDefaults: true, module})
 }
 
 module.exports = { getConfig }

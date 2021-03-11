458ef8c3e823813303c30a67900f7a9535598402
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-11 09:20:31 +0100

    fix: svite config

--- configs/svite-routify.config.js
+++ configs/svite-routify.config.js
@@ -3,16 +3,25 @@ module.exports = {
     supersedes: ['routify2', 'routify'],
     condition: ({ pkgjson }) => pkgjson.dependencies['@svitejs/vite-plugin-svelte'],
     config: () => {
-        const { readFileSync } = require('fs')
-        const html = readFileSync('./dist/index.html', 'utf8')
-        const script = html.match(/<script .+?src="([^"]+)"/)[1]
-        return {
+        const config = {
             sitemap: '.routify/urlIndex.json',
-            script: `dist${script}`,
             entrypoint: 'dist/index.html',
             inlineDynamicImports: true,
             outputDir: 'dist',
             eventName: 'app-loaded',
         }
+
+        const script = getScript(config.entrypoint)
+        if (script)
+            config.script = `dist${script}`
+
+        return config
     }
+}
+
+function getScript(entrypoint) {
+    const { readFileSync, existsSync } = require('fs')
+    if (existsSync(entrypoint))
+        return readFileSync(entrypoint, 'utf8')
+            .match(/<script .+?src="([^"]+)"/)[1]
 }

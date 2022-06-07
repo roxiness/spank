aeb956097fa3b9ad029172df0ead74f0a5d9f142
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-06-07 19:40:03 +0200

    fix: better handling of routify 3 native

---
+++ configs/routify3native.config.js
@@ -0,0 +1,27 @@
+const { findFirstPath } = require('../utils')
+
+module.exports = {
+    name: 'Routify 3 Native',
+    condition: ({ pkgjson }) => {
+        const isRoutify = pkgjson.dependencies['@roxi/routify']
+        return isRoutify && require('fs-extra').existsSync('dist/server/App.js')
+    },
+    config: () => {
+        return {
+            renderer: 'routify3native',
+            sitemap: ['/index'],
+            outputDir: 'dist/client',
+            template: findFirstPath([
+                'dist/client/__app.html',
+                'dist/client/index.html',
+                'dist/index.__app',
+                'dist/index.html',
+                'index.html',
+                'src/index.html',
+            ]),
+            script: './dist/server/App.js',
+            eventName: 'app-loaded',
+            inlineDynamicImports: false,
+        }
+    },
+}

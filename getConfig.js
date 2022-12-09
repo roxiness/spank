e24f31c7bc2ce7442ad1e2e631a61d21132cc904
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-12-10 00:15:07 +0100

    fix: bump configent

--- getConfig.js
+++ getConfig.js
@@ -5,7 +5,10 @@ import defaults from './defaults.js'
 export const getConfig = async input => {
     return {
         ...defaults,
-        ...await configent({ useDetectDefaults: true }),
+        ...(await configent({
+            useDetectDefaults: true,
+            consumerDir: import.meta.url,
+        })),
         ...input,
     }
 }
